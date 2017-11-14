5f55eb432bcc56b05ff5bb4f05dbd5caa476e2a3
Ariel Aizemberg <aaizemberg@users.noreply.github.com>
2017-11-14 15:40:41 -0300

    Update app.js

--- xdata/app.js
+++ xdata/app.js
@@ -1,8 +1,11 @@
 var data = [
   {"fecha":"2017-11-10","titulo":"Las noticias de la semana [28/oct .. 3/nov]","imagen":"https://pbs.twimg.com/media/DORrXh2WAAAKPxq.jpg","url":"https://twitter.com/aaizemberg/status/928985695611432960"},
-  {"fecha":"2017-11-08","titulo":"1.495.952 viajes en @BAecobici, desde el 1/1 al 31/10 del 2017 sobre un heatmap","imagen":"https://pbs.twimg.com/media/DOJGZVwXUAETzDR.jpg","url":"https://twitter.com/aaizemberg/status/928382621549199360"},
-  {"fecha":"2017-11-07","titulo":"Leyendo feeds rss desde google colab python interactive notebooks","imagen":"https://pbs.twimg.com/media/DOC0iXWW0AADPHF.jpg","url":"https://twitter.com/aaizemberg/status/927940921978773504"}
-];
+{"fecha":"2017-11-08","titulo":"1.495.952 viajes en @BAecobici, desde el 1/1 al 31/10 del 2017 sobre un heatmap","imagen":"https://pbs.twimg.com/media/DOJGZVwXUAETzDR.jpg","url":"https://twitter.com/aaizemberg/status/928382621549199360"},
+{"fecha":"2017-11-07","titulo":"Leyendo feeds rss desde google colab python interactive notebooks","imagen":"https://pbs.twimg.com/media/DOC0iXWW0AADPHF.jpg","url":"https://twitter.com/aaizemberg/status/927940921978773504"},
+{"fecha":"2017-09-26","titulo":"Lanzamiento de la Lic. en Analítica Empresarial y Social","imagen":"img/av_bigdata_itba_wordcloud.jpg","url":"https://www.itba.edu.ar/agenda/lanzamiento-nueva-carrera-de-grado/"},
+{"fecha":"2016-05-18","titulo":"Big Data en Argentina: presente y futuro","imagen":"https://pbs.twimg.com/media/CivY6ETXIAAOPjr.jpg","url":"img/Agenda_BigData_PyF_2016.jpg"},
+{"fecha":"2014-05-06","titulo":"La Revolución Big Data: nuevas formas de pensar y trabajar","imagen":"img/itba-sede.jpg","url":"https://www.clarin.com/tech/revolucion-big-data-pensar-trabajar_0_B1-cFAKDQg.html"}
+           ];
 
 var figures = d3.select("div#news").selectAll("figure").data(data).enter().append("figure");
 
